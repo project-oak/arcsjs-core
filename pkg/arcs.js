6d033e4adb3651e7cfa955496a3b576b640acc47
Scott J. Miles <sjmiles@google.com>
2022-01-21 15:29:01 -0800

    remove arcs.js
    Change-Id: Iabc6ecc7931d0f6eda3a4e866f461eecc101a42a

--- pkg/arcs.js
+++
@@ -1,8 +0,0 @@
-export * from './js/Runtime.js';
-export * from './js/core/Arc.js';
-export * from './js/core/Host.js';
-export * from './js/core/Decorator.js';
-export * from './js/recipe/Chef.js';
-export * from './js/isolation/ses.js';
-export * as utils from './js/utils/utils.js';
-export {logFactory} from './js/utils/utils.js';
