0b3f750039b7937dedb15bd887c5b1e275a0dca6
Sarah Heimlich <sarahheimlich@google.com>
2022-01-24 13:09:08 +1100

    Add BSD license.
    Change-Id: I525f76610eba0f733ef83c70e28f8785174430ed

--- pkg/ts/Runtime.ts
+++ pkg/ts/Runtime.ts
@@ -1,11 +1,7 @@
 /**
- * @license
- * Copyright 2021 Google LLC.
- * This code may only be used under the BSD style license found at
- * http://polymer.github.io/LICENSE.txt
- * Code distributed by Google as part of this project is also
- * subject to an additional IP rights grant found at
- * http://polymer.github.io/PATENTS.txt
+ * Copyright (c) 2022 Google LLC All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
  */
 import {Arc} from './core/Arc.js';
 import {Host} from './core/Host.js';
