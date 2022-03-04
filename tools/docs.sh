6a895c8be8b9f7bcfa7d8d9d1aa9a97763992df4
Sarah Heimlich <sarahheimlich@google.com>
2022-03-04 16:07:36 +1100

    Update license.
    Change-Id: I4f38aace7523317ff452c3f33acbd97ddb2098f8

--- tools/docs.sh
+++ tools/docs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright (c) 2022 Google LLC All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 #npx typedoc #--theme ./tools/typedoc-themes/arcs
 
 npx typedoc ./pkg/ts/*.ts ./pkg/ts/**/*.ts --out ./pkg/docs
