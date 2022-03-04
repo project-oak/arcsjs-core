18e1752f3a0b77dbc055a3ee0cab2edb180982cd
Sarah Heimlich <sarahheimlich@google.com>
2022-03-04 16:30:20 +1100

    Fixing license formatting.
    Change-Id: If1d9c56b67f6da114bb4966aee826ce1d48928e6

--- tools/bundle.sh
+++ tools/bundle.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# Copyright (c) 2022 Google LLC All rights reserved.
+# Copyright 2022 Google LLC
+# 
 # Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
 
 echo ':: npx esbuild'
 npx esbuild ./pkg/arcs.ts --bundle --minify --sourcemap --target=esnext --format=esm --outfile=./pkg/arcs.min.js
--- tools/docs.sh
+++ tools/docs.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# Copyright (c) 2022 Google LLC All rights reserved.
+# Copyright 2022 Google LLC
+# 
 # Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
 
 #npx typedoc #--theme ./tools/typedoc-themes/arcs
 
