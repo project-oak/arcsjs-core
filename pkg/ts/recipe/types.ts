0ad48e19e99d276e476a914270eb6d7af3904cc1
Maria Kleiner <mmandlis@chromium.org>
2022-04-26 22:40:38 -0700

    mark $inputs and $outputs optional

--- pkg/ts/recipe/types.ts
+++ pkg/ts/recipe/types.ts
@@ -51,8 +51,8 @@ export type Plan = {stores: StoreSpec[]};
 export type ParticleSpec = {
   $kind: string,
   $bindings?: pojo,
-  $inputs: pojo,
-  $outputs: pojo,
+  $inputs?: pojo,
+  $outputs?: pojo,
   $staticInputs?: pojo,
   $container: string,
   $slots?: Dictionary<SlotSpec>,
