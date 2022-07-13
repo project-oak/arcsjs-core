d0ad349c37a17bb502bde6f0f288d20b3d2439ed
Maria Kleiner <mmandlis@chromium.org>
2022-07-13 12:57:50 -0700

    add tests to HostInputsChangeTests (#24)
    * Update HostInputsChangeTests.js
    
    * Update HostInputsChangeTests.js
    
    * Update HostInputsChangeTests.js
    
    * Update HostInputsChangeTests.js
    
    * Update HostInputsChangeTests.js
    
    * Update HostInputsChangeTests.js
    
    * Update HostInputsChangeTests.js

--- pkg/tests/specs/HostInputsChangeTests.js
+++ pkg/tests/specs/HostInputsChangeTests.js
@@ -50,18 +50,17 @@ const AppClass = class {
     // add `host` to `arc`
     await arc.addHost(host);
     // track when the host signals a change
-    const changes = [];
+    this.changes = [];
     host.listen(
       'inputs-changed',
       () => {
         //sysLog('Host:inputs-changed:', JSON.stringify(host.particle.inputs, null, '  '));
-        changes.push(utils.deepCopy(host.particle.inputs));
+        this.changes.push(utils.deepCopy(host.particle.inputs));
       },
       'ahoy'
     );
-    changes.disposeListener = () => host.unlisten('inputs-changed', 'ahoy');
-    changes.finalize = () => (changes.disposeListener(), [...changes]);
-    return changes;
+    this.changes.disposeListener = () => host.unlisten('inputs-changed', 'ahoy');
+    this.changes.finalize = () => (this.changes.disposeListener(), [...this.changes]);
   }
 };
 
@@ -71,7 +70,7 @@ export const hostInputsChangeTest = async () => {
   // create a test application
   const app = new AppClass();
   // test application will observe onStoreChange events sent to Host (?)
-  const changes = await app.init(store);
+  await app.init(store);
   // set a key: value pair twice, second change should be filtered
   store.set('b', 42);
   store.set('b', 42);
@@ -88,35 +87,154 @@ export const hostInputsChangeTest = async () => {
   store.set('arr', arr);
   arr.push(3);
   store.set('arr', arr);
-  // capture operations
   await waitFor(100);
-  const actualChanges = changes.finalize();
-  // define expectations
+  const actualChanges = app.changes.finalize();
   const expectedChanges = [
-    {value: {
-      b: 42
-    }},
-    {value: {
-      b: 42,
-      obj: {things: 7}
-    }},
-    {value: {
-      b: 42,
-      obj: {things: 6}
-    }},
-    {value: {
-      b: 42,
-      obj: {things: 6},
-      arr: [0, 1, 2]
-    }},
-    {value: {
-      b: 42,
-      obj: {things: 6},
-      arr: [0, 1, 2, 3]
-    }}
+    {value: {'b': 42}},
+    {value: {'b': 42, 'obj': {things: 7}}},
+    {value: {'b': 42, 'obj': {things: 6}}},
+    {value: {'b': 42, 'obj': {things: 6}, arr: [0, 1, 2]}},
+    {value: {'b': 42, 'obj': {things: 6}, arr: [0, 1, 2, 3]}}
   ];
-  return await checkState(
-    actualChanges,
-    expectedChanges
-  );
+  return await checkState(actualChanges, expectedChanges);
+};
+
+export const hostInputChangeTest_fieldChange_ignore = async () => {
+  const store = new Store();
+  const app = new AppClass();
+  await app.init(store);
+
+  const object = {hello: 'world'};
+  store.set('object', object);
+
+  // A field in an unchanged object is set, no update.
+  object.foo = 'bar';
+  store.set('object', object);
+
+  await waitFor(100);
+  const actualChanges = app.changes.finalize();
+  const expectedChanges = [
+    {value: {'object': {hello: 'world'}}}
+  ];
+  return await checkState(actualChanges, expectedChanges);
+};
+
+export const hostInputChangeTest_clonedObjectFieldChange_capture = async () => {
+  const store = new Store();
+  const app = new AppClass();
+  await app.init(store);
+
+  const object = {hello: 'world'};
+  store.set('object', object);
+
+  // Object is cloned and a field.
+  store.set('object', {...object, foo: 'bar'});
+
+  await waitFor(100);
+  const actualChanges = app.changes.finalize();
+  const expectedChanges = [
+    {value: {'object': {hello: 'world'}}},
+    {value: {'object': {hello: 'world', foo: 'bar'}}}
+  ];
+  return await checkState(actualChanges, expectedChanges);
+};
+
+export const hostInputChangeTest_clonedObjectNestedFieldChange_ignore = async () => {
+  const store = new Store();
+  const app = new AppClass();
+  await app.init(store);
+
+  const object = {hello: {'hi': 'hola'}};
+  store.set('object', object);
+
+  // Clone the object and change a nested field.
+  const cloned = {...object};
+  cloned.hello.hi = 'shalom';
+
+  store.set('object', cloned);
+
+  await waitFor(100);
+  const actualChanges = app.changes.finalize();
+  const expectedChanges = [
+    {value: {'object': {hello: {'hi': 'hola'}}}}
+  ];
+  return await checkState(actualChanges, expectedChanges);
+};
+
+export const hostInputChangeTest_listUpdate_ignored = async () => {
+  const store = new Store();
+  const app = new AppClass();
+  await app.init(store);
+
+  const list = ['one', 'two', 'three'];
+  store.set('list', list);
+
+  list[1] = 'two two';
+  store.set('list', list);
+  list.push('four');
+  store.set('list', list);
+
+  await waitFor(100);
+  const actualChanges = app.changes.finalize();
+  const expectedChanges = [
+    {value: {'list': ['one', 'two', 'three']}}
+  ];
+  return await checkState(actualChanges, expectedChanges);
+};
+
+export const hostInputChangeTest_clonedListUpdate_captured = async () => {
+  const store = new Store();
+  const app = new AppClass();
+  await app.init(store);
+
+  const list = ['one', 'two', 'three'];
+  store.set('list', list);
+
+  store.set('list', [...list, 'four']);
+
+  await waitFor(100);
+  const actualChanges = app.changes.finalize();
+  const expectedChanges = [
+    {value: {'list': ['one', 'two', 'three']}},
+    {value: {'list': ['one', 'two', 'three', 'four']}}
+  ];
+  return await checkState(actualChanges, expectedChanges);
+};
+
+export const hostInputChangeTest_clonedNestedListUpdate_ignored = async () => {
+  const store = new Store();
+  const app = new AppClass();
+  await app.init(store);
+
+  const object = {'one': 1, 'two': [2, 22, 222], three: 3};
+  store.set('object', object);
+
+  object.two.push(2222);
+  store.set('object', {...object});
+
+  await waitFor(100);
+  const actualChanges = app.changes.finalize();
+  const expectedChanges = [
+    {value: {'object': {'one': 1, 'two': [2, 22, 222], three: 3}}}
+  ];
+  return await checkState(actualChanges, expectedChanges);
+};
+
+export const hostInputChangeTest_clonedNestedListUpdate_captured = async () => {
+  const store = new Store();
+  const app = new AppClass();
+  await app.init(store);
+
+  const object = {'one': 1, 'two': [2, 22, 222], three: 3};
+  store.set('object', object);
+
+  store.set('object', {...object, two: [...object.two, 2222]});
+
+  await waitFor(100);
+  const actualChanges = app.changes.finalize();
+  const expectedChanges = [
+    {value: {'object': {'one': 1, 'two': [2, 22, 222], three: 3}}},
+    {value: {'object': {'one': 1, 'two': [2, 22, 222, 2222], three: 3}}}
+  ];
+  return await checkState(actualChanges, expectedChanges);
 };
