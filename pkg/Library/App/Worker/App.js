3a12624191ca69fc0bdd98967b335d76f30b70cf
Jing Jin <8752427+jinjingforever@users.noreply.github.com>
2022-08-23 15:07:56 -0700

    Add a missing `await`

--- pkg/Library/App/Worker/App.js
+++ pkg/Library/App/Worker/App.js
@@ -51,7 +51,7 @@ export const App = class {
         return this.restore(request);
     }
     const result =
-      this.dispatchServiceRequest({request})
+      await this.dispatchServiceRequest({request})
       || this.appService({request})
       ;
     return await result;
