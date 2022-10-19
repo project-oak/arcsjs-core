9b92d75ded29c656ff772b4175b88a775f2a4155
Maria Kleiner <mmandlis@chromium.org>
2022-10-19 00:38:46 -0700

    show json parsing errors, and introduce simplified spec (#152)

--- pkg/nodegraph/Library/Librarian/CustomParticle.js
+++ pkg/nodegraph/Library/Librarian/CustomParticle.js
@@ -14,18 +14,21 @@ async onCodeChanged({eventlet: {key, value}, pipeline, nodeId}) {
   return {pipeline};
 },
 
-onSpecChanged({eventlet: {value}, pipeline, nodeId}) {
-  const custom = this.requireCustom(pipeline, nodeId);
+onSpecChanged({eventlet: {value}, pipeline, nodeId}, state) {
   try {
-    const spec = JSON.parse(value);
-    if (spec.$meta) {
-      assign(spec.$meta, {id: nodeId, category: 'Custom', custom: true});
+    const parsed = JSON.parse(value);
+    state.error = '';
+    const spec = this.simplifiedToSpec(parsed, nodeId);
+    if (spec) {
+      const custom = this.requireCustom(pipeline, nodeId);
+      custom.spec = spec;
+      return {pipeline};
     }
-    custom.spec = spec;
   } catch (e) {
-    log(`Failed parsing spec with error ${e}`);
+    state.error = `Failed parsing spec with error ${e}`;
+    log(state.error);
+
   }
-  return {pipeline};
 },
 
 requireCustom(pipeline, nodeId) {
@@ -36,18 +39,45 @@ requireCustom(pipeline, nodeId) {
 
 async onUpdate() {},
 
-render({pipeline, nodeId}) {
-  if (pipeline?.custom?.[nodeId]) {
-    return this.renderCustom(pipeline.custom[nodeId]);
-  }
+render({pipeline, nodeId}, state) {
+  const custom = pipeline?.custom?.[nodeId];
+  return {
+    html: custom?.html || '',
+    js: custom?.js || '',
+    meta: JSON.stringify(this.specToSimplified(custom?.spec), null, 2),
+    error: state.error
+  };
 },
 
-renderCustom({html, js, spec}) {
+specToSimplified(spec) {
+  const notKeyword = name => !name.startsWith('$');
+  const particle = spec?.[keys(spec).filter(notKeyword)?.[0]];
   return {
-    html,
-    js,
-    specStr: JSON.stringify(spec, null, 2)
+    displayName: spec?.$meta?.displayName || '',
+    category: spec?.$meta?.category || 'Custom',
+    $stores: spec?.$stores || {},
+    $inputs: particle?.$inputs || [],
+    $outputs: particle?.$outputs || []
   };
+},
+
+simplifiedToSpec({displayName, category, $stores, $inputs, $outputs}, nodeId) {
+  if (displayName) {
+    return {
+      $meta: {
+        id: nodeId,
+        displayName,
+        category,
+        custom: true
+      },
+      $stores,
+      particle: {
+        $kind: nodeId,
+        $inputs,
+        $outputs
+      }
+    };
+  }
 },
 
 template: html`
@@ -73,17 +103,24 @@ template: html`
   [full] {
     height: 100%;
   }
+  [error] {
+    color: red;
+    padding: 10px;
+    white-space: normal;
+  }
 </style>
 
 <div full>
   <div bar>
-    <div flex></div>
+    <div flex>
+      <div error>{{error}}</div>
+    </div>
     <mwc-button raised on-click="onUpdate">Update</mwc-button>
   </div>
   <mxc-tab-pages flex full tabs="Html, Js, Meta">
     <code-mirror flex text="{{html}}" key="html" on-code-blur="onCodeChanged"></code-mirror>
     <code-mirror flex text="{{js}}" key="js" on-code-blur="onCodeChanged"></code-mirror>
-    <code-mirror flex text="{{specStr}}" on-code-blur="onSpecChanged"></code-mirror>
+    <code-mirror flex text="{{meta}}" on-code-blur="onSpecChanged"></code-mirror>
   </mxc-tab-pages>
 </div>
 `
