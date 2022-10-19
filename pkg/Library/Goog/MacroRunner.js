b4eacfc19b3cba857e6ed9c47807923d65997e4a
Scott J. Miles <sjmiles@google.com>
2022-10-19 16:19:22 -0700

    remove moar

--- pkg/Library/Goog/MacroRunner.js
+++ pkg/Library/Goog/MacroRunner.js
@@ -3,9 +3,7 @@
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
-
 ({
-
 async update(inputs, state, {service, output}) {
   if (this.canRunMacro(inputs) && this.inputsChanged(inputs, state)) {
     assign(state, {...inputs});
