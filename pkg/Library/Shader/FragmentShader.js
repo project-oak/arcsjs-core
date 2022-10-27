d5abc53789afabc63cca0a88f410b1280a814668
Scott J. Miles <sjmiles@google.com>
2022-10-26 18:03:08 -0700

    Update FragmentShader.js

--- pkg/Library/Shader/FragmentShader.js
+++ pkg/Library/Shader/FragmentShader.js
@@ -8,7 +8,7 @@
   async initialize({}, state, {service}) {
     state.canvas = await service({kind: 'ThreejsService', msg: 'allocateCanvas'});
   },
-  async update(inputs, state, {service, invalidate}) {
+  async update(inputs, state, {service}) {
     //log(inputs.image);
     const {shader, image, audio} = inputs;
     if (shader !== state.shader) {
