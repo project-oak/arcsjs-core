a20ff98ad8a6679acc6f35069e725c4d09b7bdbf
Scott J. Miles <sjmiles@google.com>
2022-11-01 12:12:25 -0700

    use simplified app structure
    This is the latest app structure, I’m not sure it’s better. WDYT?

--- pkg/graphs/conf/config.js
+++ pkg/graphs/conf/config.js
@@ -1,14 +1,11 @@
-// extract an absolute url to the folder 2 above here (counting the filename, aka '/')
-import {Paths} from '../../Library/Core/utils.min.js';
-const url = Paths.getAbsoluteHereUrl(import.meta, 3);
-
 globalThis.config = {
-  arcsPath: url,
+  arcsPath: '..',
   aeon: 'graphs/0.0.1',
   meta: 'graphs',
   // theme: 'dark',
   logFlags: {
     app: true,
+    arcs: true,
     //recipe: true,
     //arc: true,
     particles: true,
