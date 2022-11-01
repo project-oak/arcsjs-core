56c40ef04ba49baba87553328ab4faab534e9da7
Scott J. Miles <sjmiles@google.com>
2022-11-01 13:36:46 -0700

    use simplified app structure (#176)
    This is the latest app structure, I’m not sure it’s better. WDYT?

--- pkg/graphs/conf/arcs.js
+++ pkg/graphs/conf/arcs.js
@@ -8,6 +8,7 @@ import 'https://arcsjs.web.app/lib/corsFix.js';
 export * from '../../Library/TensorFlow/TensorFlow.js';
 export * from '../../Library/Core/utils.min.js';
 export * from '../../Library/App/surface-imports.js';
+export * from '../../Library/App/boot.js';
 // export * from '../../Library/Dom/container-layout.js';
 export * from '../../Library/Designer/designer-layout.js';
 export * from '../../Library/Dom/multi-select.js';
@@ -16,17 +17,3 @@ export * from '../../Library/Dom/multi-select.js';
 export * from '../../Library/Threejs/threejs-editor.js';
 //export * from '../../Library/AFrame/aframe.js';
 export * from '../../Library/PixiJs/pixi-view.js';
-
-// n.b. operates in outer context
-
-// extract an absolute url to the folder 2 above here (counting the filename, aka 'nodegraph/')
-import {Paths} from '../../Library/Core/utils.min.js';
-const url = Paths.getAbsoluteHereUrl(import.meta, 2);
-
-// calculate important paths
-export const paths = {
-  $app: url,
-  $graphs: `${url}/Library`,
-  $config: `${url}/conf/config.js`,
-  $library: `${url}/../Library`
-};
