bc402a942f1e8869d576126d6ff65ec11ae304c0
Maria Kleiner <mmandlis@chromium.org>
2022-11-02 18:04:24 -0700

    wip db app

---
+++ pkg/database/Library/DbHome.js
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file or at
+ * https://developers.google.com/open-source/licenses/bsd
+ */
+({
+template: html`
+<style>
+  :host {
+    color: var(--theme-color-fg-1);
+    background-color: var(--theme-color-bg-0);
+    --mdc-icon-button-size: 24px;
+    --mdc-icon-size: 16px;
+    padding: 100px;
+    ${globalThis.themeRules}
+  }
+  [frame="recordsViewer"] {
+    border: 1px solid purple;
+    padding: 10px;
+  }
+</style>
+
+<div center><b><i>WELCOME  TO   ARCS   DATABASE</i></b></div>
+<br>
+<div frame="recordsViewer"></div>
+<br>
+
+`
+});
