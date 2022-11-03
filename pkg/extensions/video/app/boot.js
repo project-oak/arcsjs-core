b0e7dedc12134bdc550fcde8b23ef579d00b35cb
Scott J. Miles <sjmiles@google.com>
2022-11-03 14:42:25 -0700

    move various properties to labs

--- pkg/extensions/video/app/boot.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright (c) 2022 Google LLC All rights reserved.
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
- */
-import "./index.js";
-
-globalThis.app.arcs.setComposerRoot(document.body);
