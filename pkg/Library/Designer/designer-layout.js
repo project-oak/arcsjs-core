41d818bf68ba9fd4bc99d1e337fff98edb8ab76e
Scott J. Miles <sjmiles@google.com>
2022-11-07 21:38:11 -0800

    Update designer-layout.js

--- pkg/Library/Designer/designer-layout.js
+++ pkg/Library/Designer/designer-layout.js
@@ -29,16 +29,18 @@ export class DesignerLayout extends DragDrop {
     this.updateGeometry();
   }
   updateGeometry() {
-    this.select(null);
-    const map = {};
-    this.rects?.forEach(r => map[this.sanitizeId(r.id)] = r);
-    for (const child of this.children) {
-      if (child !== this.target) {
-        const rect = map[child.id]?.position || {l: 64, t: 64, w: 240, h: 180};
-        this.position(child.id, rect);
+    if (!this.dragging) {
+      this.select(null);
+      const map = {};
+      this.rects?.forEach(r => map[this.sanitizeId(r.id)] = r);
+      for (const child of this.children) {
+        //if (child !== this.target) {
+          const rect = map[child.id]?.position || {l: 64, t: 64, w: 240, h: 180};
+          this.position(child.id, rect);
+        //}
       }
+      this.selectAll(this.selected);
     }
-    this.selectAll(this.selected);
   }
   position(id, position) {
     const child = this.getChildById(id);
