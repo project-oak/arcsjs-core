3b5a1b33ea3a674374f031b29e59081307a8a827
Scott J. Miles <sjmiles@google.com>
2022-11-07 21:45:04 -0800

    more cleanup

--- pkg/Library/Display/JsonViewer.js
+++ pkg/Library/Display/JsonViewer.js
@@ -1,10 +1,10 @@
-({
 /**
  * @license
  * Copyright (c) 2022 Google LLC All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
+({
 render({classifierResults}) {
   const hasResults = classifierResults?.length > 0;
   return {
