b8d53a53a250c570a7909bbf876f066bdd01f444
Scott J. Miles <sjmiles@google.com>
2022-12-05 14:40:57 -0800

    add UX for choosing from Pixabay results (#205)

--- pkg/Library/Pixabay/Pixabay.js
+++ pkg/Library/Pixabay/Pixabay.js
@@ -8,7 +8,7 @@
 async update({query, index}, state) {
   if (query && !state.query !== query) {
     state.query = query;
-    state.image =await this.requestImages(query, index);
+    state.image = await this.requestImages(query, index);
   }
 },
 async requestImages(query, index) {
@@ -17,7 +17,7 @@ async requestImages(query, index) {
   const json = await response.json();
   log(json);
   const hits = json?.hits;
-  const url = hits?.[index > 0 ? index : 0]?.largeImageURL;
+  const url = hits?.[index > 0 ? index : 0]?.webformatURL;
   return {hits, url}
 },
 onCanvas({eventlet: {value}}, state) {
@@ -26,12 +26,38 @@ onCanvas({eventlet: {value}}, state) {
     return {image};
   }
 },
+onPrevClick(inputs, state) {
+  const image = state.image = this.walkImage(-1, state);
+  return {image};
+},
+onNextClick(inputs, state) {
+  const image = state.image = this.walkImage(1, state);
+  return {image};
+},
+walkImage(delta, {image}) {
+  if (image) {
+    return {
+      ...image,
+      canvas: null,
+      url: this.walkUrl(image, delta),
+      version: Math.random()
+    };
+  }
+},
+walkUrl({hits, url}, delta) {
+  if (hits) {
+    const current = hits.findIndex(({webformatURL}) => webformatURL === url);
+    const index = ((current ?? 0) + delta) % hits.length;
+    return hits[index]?.webformatURL;
+  }
+},
 template: html`
 <style>
   image-resource {
     background: transparent;
   }
 </style>
+<div toolbar><icon on-click="onPrevClick">arrow_circle_left</icon><icon on-click="onNextClick">arrow_circle_right</icon></div>
 <image-resource center flex image="{{image}}" on-canvas="onCanvas"></image-resource>
 `
 })
