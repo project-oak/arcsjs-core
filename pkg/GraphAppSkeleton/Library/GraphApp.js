eb6ba9a26c2165c504acc6b1ccec3e41682d4d6f
Scott J. Miles <sjmiles@google.com>
2022-12-16 16:47:15 -0800

    Update GraphApp.js

--- pkg/GraphAppSkeleton/Library/GraphApp.js
+++ pkg/GraphAppSkeleton/Library/GraphApp.js
@@ -4,7 +4,7 @@
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
-import {logFactory, App, RecipeBuilder} from '../arcs/arcs.js';
+import {App, RecipeBuilder} from '../arcs/arcs.js';
 import {GraphRecipe} from './GraphRecipe.js';
 
 export const GraphApp = class extends App {
@@ -38,41 +38,6 @@ export const GraphApp = class extends App {
     }
     const layout = graph?.position?.previewLayout;
     const recipes = RecipeBuilder.construct({graph, layout, nodeTypes: this.nodeTypes});
-    //
-    // this.lastRecipes = recipes;
-    // logGraphInfo(graph, recipes, nodeTypes);
-    // //
-    // if (!this.layout) {
-    //   // first time
-    //   this.lastRecipes = null;
-    //   log(recipes);
-    //   this.layout = layout;
-    //   this.arcs.set('user', 'graphLayout', this.layout);
-    // }
-    //
     await this.arcs.addRecipes('user', recipes);
-    //
-    // TODO(sjmiles): MutationObserver only works 50% of the time :(
-    // const fixLayout = () => {
-    //   const dlayout = deepQuerySelector(document.body, 'designer-layout');
-    //   if (!dlayout) {
-    //     setTimeout(fixLayout, 100);
-    //   } else {
-    //     layout.kick = Math.random()
-    //     setTimeout(() => layout.kick = Math.random(), 300);
-    //   }
-    // };
-    // setTimeout(fixLayout, 100);
   }
 };
-
-// const logGraphInfo = (graph, recipes, nodeTypes) => {
-//   log.groupCollapsed('addGraph');
-//   log(graph);
-//   log('constructed recipes:');
-//     log.groupCollapsed('from nodeTypes:');
-//     log.log(JSON.stringify(nodeTypes, null, '  '));
-//     log.groupEnd();
-//   log(JSON.stringify(recipes, null, '  '));
-//   log.groupEnd();
-// };
