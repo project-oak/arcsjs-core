44b70deabb9e0d113aa21e80a67e899dcdccade7
Scott J. Miles <sjmiles@google.com>
2022-12-21 23:49:47 -0800

    Update nodeTypes.js

--- pkg/Apps/CommonApp/arcs/nodeTypes.js
+++ pkg/Apps/CommonApp/arcs/nodeTypes.js
@@ -4,6 +4,5 @@
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
-//export {nodeTypes, categories} from '../../../Library/NodeTypes/nodeTypes.js';
 const {nodeTypes, categories} = await import(`${globalThis.config.arcsPath}/Library/NodeTypes/nodeTypes.js`);
 export {nodeTypes, categories};
