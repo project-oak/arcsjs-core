87de74725fdd5ebf03066efbf8eadb231827a46a
Scott J. Miles <sjmiles@google.com>
2023-01-15 20:50:28 -0800

    GraphRunner runs GraphMaker Graphs (incl. GraphMaker)

--- pkg/Library/App/arcsjs-app-surface.js
+++ pkg/Library/App/arcsjs-app-surface.js
@@ -6,14 +6,18 @@
  */
 import {Xen} from '../Dom/Xen/xen-async.js';
 import {dom, loadScript} from '../Dom/Dom.js';
-import '../Mediapipe/PoseServiceLoader.js';
 import {themeRules} from './theme.js';
 import {boot} from './Common/boot.js';
-import {graph} from '../../Apps/GraphRunner/graph.js';
+import '../Mediapipe/PoseServiceLoader.js';
+import {Params} from './Params.js';
+//import {graph} from '../../Apps/GraphRunner/graph.js';
 
 const Library = `${globalThis.config?.arcsPath}/Library`;
 
 export const ArcsJsApp = class extends Xen.Async {
+  static getObservedAttributes() {
+    return ['path'];
+  }
   get host() {
     return this;
   }
@@ -22,7 +26,21 @@ export const ArcsJsApp = class extends Xen.Async {
     // loadScript(`${Library}/Mediapipe/PoseServiceLoader.js`);
     loadScript(`${Library}/../third_party/pixijs/pixi.6.5.7.min.js`);
     //loadScript(`${library}/../third_party/pixijs/pixi-plugins/pixi-spine.js`);
-    boot(import.meta.url, {graph});
+    //
+    // const graphs = JSON.parse(localStorage.getItem('user/graph/0.4.5/graphs'));
+    // const graph = graphs.find(g => g.$meta?.name === 'idolized-voice');
+    // boot(import.meta.url, {graph});
+  }
+  update({path}, state) {
+    if (!state.running) {
+      const name = path ?? Params.getParam('path');
+      const graphs = JSON.parse(localStorage.getItem('user/graph/0.4.5/graphs'));
+      const graph = graphs.find(g => g?.$meta?.name === name);
+      if (graph) {
+        state.running = true;
+        boot(import.meta.url, {graph});
+      }
+    }
   }
   get template() {
     return Xen.html`
@@ -30,7 +48,6 @@ export const ArcsJsApp = class extends Xen.Async {
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <link rel="stylesheet" href="${Library}/Dom/Material/material-icon-font/icons.css">
-<title>ArcsJsApp! An ArcsJs Graph</title>
 <style>
   html, body {
     height: 100%;
