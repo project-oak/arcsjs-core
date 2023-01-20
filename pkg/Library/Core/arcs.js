8c9cc8516a02e922a735bf6cf566cd172a5df1ed
Scott J. Miles <sjmiles@google.com>
2023-01-19 23:22:39 -0800

    update build

--- pkg/Library/Core/arcs.js
+++ pkg/Library/Core/arcs.js
@@ -1532,16 +1532,16 @@ export {
 };
 /**
  * @license
+ * Copyright (c) 2022 Google LLC All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+/**
+ * @license
  * Copyright 2022 Google LLC
  *
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file or at
  * https://developers.google.com/open-source/licenses/bsd
- */
-/**
- * @license
- * Copyright (c) 2022 Google LLC All rights reserved.
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
  */
 //# sourceMappingURL=arcs.js.map
