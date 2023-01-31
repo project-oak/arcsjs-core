4c45ff4b0290e70c3dc5c470c483d6e184eb19bb
Maria Kleiner <mmandlis@chromium.org>
2023-01-30 23:58:10 -0800

    add extensions support (#247)

---
+++ extensions/video/app/ExtApp.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright (c) 2022 Google LLC All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+import {
+  Paths, App, LocalStoragePersistor,
+  HistoryService,
+  ThreejsService,
+  ShaderService,
+  TensorFlowService,
+  SelfieSegmentationService,
+  MediaService
+} from './arcs.js';
+import {ExtRecipe} from './ExtRecipe.js';
+
+export const ExtApp = class extends App {
+  constructor() {
+    super(Paths.map);
+    this.services = {HistoryService, ThreejsService, ShaderService, TensorFlowService, SelfieSegmentationService, MediaService};
+    this.userAssembly = [ExtRecipe];
+    this.persistor = new LocalStoragePersistor('user');
+  }
+};
